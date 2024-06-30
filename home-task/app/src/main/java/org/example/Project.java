e6518710a00482c69f12313b36dde6f84190e09a
Orc <orcsin@gmail.com>
2024-06-30 22:29:13 +0300

    Ver0.04

--- home-task/app/src/main/java/org/example/Project.java
+++ home-task/app/src/main/java/org/example/Project.java
@@ -1,13 +1,13 @@
-package com.test;
+package org.example;
 
 import org.apache.commons.collections4.Predicate;
 import org.apache.log4j.Logger;
 
 class Project {
 
- public String getGreeting() {
-        return "Hello, MNT Lab!";
-    }
+  public String getGreeting() {
+    return "Hello, MNT Lab!";
+  }
 
   public static void test() {
     System.out.println("test");
--- home-task/app/src/test/java/org/example/UnitTest.java
+++ home-task/app/src/test/java/org/example/UnitTest.java
@@ -1,17 +1,17 @@
-package com.test;
+package org.example;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class UnitTest {
-    @Test public void testAppHasAGreeting() {
-        Project classUnderTest = new Project();
-        assertNotNull("app should have a greeting",
-                       classUnderTest.getGreeting());
-    }
+  @Test public void testAppHasAGreeting() {
+  Project classUnderTest = new Project();
+  assertNotNull("app should have a greeting",
+                  classUnderTest.getGreeting());
+  }
 
-   @Test
-   public void test(){
-     Project.test();
-   }
+  @Test
+  public void test(){
+    Project.test();
+  }
 }
